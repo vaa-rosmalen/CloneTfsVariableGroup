1cce6c6fcd26291a667e60296228337eb65f3a41
Jacob Duijzer <jduijzer@vaa.com>
2019-01-31 10:45:13 +0100

    - Showing some more info on screen - Keeps repeating unitl exit (CTRL-C)

--- CloneTfsVariableGroup/CopyVariableGroupApp.cs
+++ CloneTfsVariableGroup/CopyVariableGroupApp.cs
@@ -20,20 +20,24 @@ namespace CloneTfsVariableGroup
         {
             try
             {
-                var project = await SelectProject(_arguments.Filter).ConfigureAwait(false);
-                var variableGroup = await SelectVariableGroup(project.Name).ConfigureAwait(false);
-                var newGroupName = GetNewGroupName();
+                while(true)
+                {
+                    StartMenu(_arguments);
+                    var project = await SelectProject(_arguments.Filter).ConfigureAwait(false);
+                    var variableGroup = await SelectVariableGroup(project.Name).ConfigureAwait(false);
+                    var newGroupName = GetNewGroupName();
 
-                Console.WriteLine(Constants.NEW_LINE);
-                Console.WriteLine($"Selected project: {project.Name}");
-                Console.WriteLine($"Selected variable group: {variableGroup.Name}");
-                Console.WriteLine($"New group name: {newGroupName}");
-                Console.WriteLine(Constants.NEW_LINE);
+                    Console.WriteLine(Constants.NEW_LINE);
+                    Console.WriteLine($"Selected project: {project.Name}");
+                    Console.WriteLine($"Selected variable group: {variableGroup.Name}");
+                    Console.WriteLine($"New group name: {newGroupName}");
+                    Console.WriteLine(Constants.NEW_LINE);
 
-                Console.WriteLine($"Are you sure you want to clone group {variableGroup.Name} into {newGroupName} (y/n)?");
-                var key = Console.ReadKey();
-                if (key.Key == ConsoleKey.Y)
-                    await _remoteTfsApi.CreateNewVariableGroup(project.Name, newGroupName, variableGroup).ConfigureAwait(false);
+                    Console.WriteLine($"Are you sure you want to clone group {variableGroup.Name} into {newGroupName} (y/n)?");
+                    var key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Y)
+                        await _remoteTfsApi.CreateNewVariableGroup(project.Name, newGroupName, variableGroup).ConfigureAwait(false);
+                }
             }
             catch (Exception)
             {
@@ -41,6 +45,16 @@ namespace CloneTfsVariableGroup
             }
 
             // TODO: try again
+        }
+
+        private void StartMenu(Arguments arguments)
+        {
+            Console.WriteLine(Constants.NEW_LINE);
+            Console.WriteLine($"TFS Instance: {arguments.TfsUrl}");
+
+            if(!string.IsNullOrEmpty(arguments.Filter))
+                Console.WriteLine($"Filter: {arguments.Filter}");
+
         }
 
         private async Task<Project> SelectProject(string filter)
@@ -66,7 +80,13 @@ namespace CloneTfsVariableGroup
 
             var input = Console.ReadLine();
             if (int.TryParse(input, out int projectNumber) && projectNumber <= projects.Count)
+            {
+                Console.WriteLine(Constants.NEW_LINE);
+                Console.WriteLine($"Selected project: {projectNumber}: {projects[projectNumber].Name}");
+
                 return projects[projectNumber];
+            }
+                
 
             throw new ArgumentException("Provided input is wrong");
         }
@@ -88,7 +108,12 @@ namespace CloneTfsVariableGroup
             Console.WriteLine("Which variable group do you want to close?");
             var inputGroup = Console.ReadLine();
             if (int.TryParse(inputGroup, out int groupNumber) && groupNumber <= variableGroups.Count && variableGroups[groupNumber] != null)
+            {
+                Console.WriteLine(Constants.NEW_LINE);
+                Console.WriteLine($"Selected group: {groupNumber}: {variableGroups[groupNumber].Name}");
                 return variableGroups[groupNumber];
+            }
+                
 
             throw new ArgumentException("Provided input is wrong");
         }
